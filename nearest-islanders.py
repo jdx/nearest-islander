23c400ab071b153e7bb2a5123f97e0183c3d2fde
Jeff Dickey <dickeytk@gmail.com>
2011-02-26 11:16:31 -0800

    Added argument reading

--- nearest-islanders.py
+++ nearest-islanders.py
@@ -1,4 +1,17 @@
-import argparse
+from optparse import OptionParser
 
-parser = argparse.ArgumentParser(description='islander filename')
-parser.add_argument('inputfile', type=file)
+def main():
+    usage = "usage: %prog inputfile"
+    parser = OptionParser(usage=usage, version="%prog 1.0")
+    parser.add_option('-v', '--verbose', action="store_true", dest="verbose")
+    (options, args) = parser.parse_args()
+    if len(args) < 1:
+        parser.error("Must give path to input file")
+        return
+    elif len(args) > 1:
+        parser.error("Too many arguments given")
+        return
+    print args
+
+if __name__ == "__main__":
+    main()

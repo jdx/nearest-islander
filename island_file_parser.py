45b0517c8334e74ce511a1a4ea178764b2dd4bbc
Jeff Dickey <dickeytk@gmail.com>
2011-02-26 11:41:14 -0800

    Finished reading of the islanders

--- island_file_parser.py
+++ island_file_parser.py
@@ -1,11 +1,16 @@
 import io
 
 def parse_island_file(island_file_path, verbose=None):
+    islanders = {}
+
     if verbose:
         print "Reading file: %s" % island_file_path
     with io.open(island_file_path, 'r') as file:
         line = file.readline()
         while line:
+            values = line.split()
+            islanders[values[0]] = (int(values[1]), int(values[2]))
             if verbose:
-                print 'parsing line: %s' % line.rstrip()
+                print 'Got islander \'%s\' at %s' % (values[0], islanders[values[0]])
             line = file.readline()
+    return islanders
